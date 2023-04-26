f08e2b696941c28994534dbf3722b493971607f5
parthmodi152 <modi.parth152@gmail.com>
2023-04-25 21:07:16 -0400

    removed branding + added custom HTML block to body

--- components/navbar.js
+++ components/navbar.js
@@ -17,30 +17,19 @@ export default function Navbar(props) {
       href: "/"
     },
     {
+      label: "Archive",
+      href: "/archive"
+    }
+  ];
+
+  const rightmenu = [
+    {
       label: "About",
       href: "/about"
     },
     {
       label: "Contact",
       href: "/contact"
-    }
-  ];
-
-  const rightmenu = [
-    {
-      label: "Archive",
-      href: "/archive"
-    },
-    {
-      label: "Pro Version",
-      href: "https://stablo-pro.web3templates.com/",
-      external: true,
-      badge: "new"
-    },
-    {
-      label: "Download",
-      href: "https://web3templates.com/templates/stablo-minimal-blog-website-template",
-      external: true
     }
   ];
 
--- lib/sanity/schemas/index.ts
+++ lib/sanity/schemas/index.ts
@@ -1,7 +1,8 @@
 import blockContent from './blockContent'
+import htmlBlock from './htmlBlock';
 import category from './category'
 import post from './post'
 import author from './author'
 import settings from './settings'
 
-export const schemaTypes = [post, author, category, settings, blockContent]
+export const schemaTypes = [post, author, category, settings, htmlBlock, blockContent]
