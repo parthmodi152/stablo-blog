cd5eaf01bbc588c084e8c015050272a54790ae6d
parthmodi152 <modi.parth152@gmail.com>
2023-07-18 06:40:21 -0400

    added getresponse email to alpilean

--- app/(website)/alpilean/alpilean.js
+++ app/(website)/alpilean/alpilean.js
@@ -1,10 +1,22 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import va from "@vercel/analytics";
 
 export default function Alpilean() {
+  const [showModal, setShowModal] = useState(false);
+  const [email, setEmail] = useState("");
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowModal(true);
+    }, 5000);
+    return () => clearTimeout(timer);
+  }, []);
+
   const affiliateLink =
     "https://hop.clickbank.net/?affiliate=pmodi152&vendor=alpilean&pg=vid";
+
   const trackEvent = async (url, eventName) => {
     try {
       // Send a "hop" event to Vercel
@@ -20,6 +32,34 @@ export default function Alpilean() {
 
     // Redirect to the affiliate link
     window.location.href = url;
+  };
+
+  const createContact = async email => {
+    try {
+      await axios.post(
+        "https://api.getresponse.com/v3/contacts",
+        {
+          email: email,
+          campaign: {
+            campaignId: "qJVBJ"
+          }
+        },
+        {
+          headers: {
+            "X-Auth-Token": "api-key as2nj1ssd0463uzqncwptsawzl1edy17"
+          }
+        }
+      );
+    } catch (error) {
+      console.error("Failed to send contact to GetResponse", error);
+    }
+  };
+
+  const handleEmailSubmit = async e => {
+    e.preventDefault();
+    const contact = await createContact(email);
+    trackEvent(affiliateLink, "hop");
+    setShowModal(false);
   };
 
   return (
@@ -113,6 +153,80 @@ export default function Alpilean() {
           </a>
         </div>
       </div>
+      {showModal && (
+        <div
+          className="fixed inset-0 z-10 overflow-y-auto"
+          aria-labelledby="modal-title"
+          role="dialog"
+          aria-modal="true">
+          <div className="flex min-h-screen items-end justify-center px-4 pb-20 pt-4 text-center sm:block sm:p-0">
+            <div
+              className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+              aria-hidden="true"></div>
+            <span
+              className="hidden sm:inline-block sm:h-screen sm:align-middle"
+              aria-hidden="true">
+              &#8203;
+            </span>
+            <div className="inline-block transform overflow-hidden rounded-lg border-4 border-red-600 bg-white text-left align-bottom shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:align-middle">
+              <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                <div className="sm:flex sm:items-start">
+                  <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                    <h3
+                      className="text-center text-2xl font-bold leading-6 text-red-600"
+                      id="modal-title">
+                      WAIT!
+                    </h3>
+                    <p className="mt-2 text-center text-xl font-bold text-blue-600">
+                      I'll Reveal My Bizarre Ice Hack To Dissolve Fat
+                      Every 24 Hours!
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                <div className="sm:flex sm:items-center">
+                  <div className="sm:w-2/5">
+                    That made Sarah Miller, a 47-year-old mom of 3
+                    from Texas, suddenly melt lbs of thick, wobbly fat
+                    from her butt, hips, and face!
+                  </div>
+                  <div className="mt-5 sm:ml-6 sm:mt-0 sm:w-3/5">
+                    <iframe
+                      src="https://fast.wistia.net/embed/iframe/szui8wui42?seo=false"
+                      title=""
+                      allowTransparency={true}
+                      allowFullScreen
+                      frameBorder="0"
+                      scrolling="no"
+                      className="h-56 w-full sm:h-auto"
+                      name="wistia_embed"></iframe>
+                  </div>
+                </div>
+                <div className="mt-5 text-gray-700">
+                  <input
+                    type="email"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+                    id="email"
+                    placeholder="Email"
+                    required
+                  />
+                </div>
+              </div>
+              <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:justify-center sm:px-6">
+                <button
+                  type="button"
+                  className="inline-flex w-full animate-pulse justify-center rounded-lg border border-blue-600 bg-yellow-500 px-6 py-3 text-lg font-semibold text-blue-800 shadow-lg hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 sm:w-auto sm:text-base"
+                  onClick={handleEmailSubmit}>
+                  Start Watching To Discover Fast-Melting Ice Hack
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
